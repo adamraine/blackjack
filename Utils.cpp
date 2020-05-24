6adb91178e43b40ac412e45c3fbbc43debde3941
Adam Raine <theoperatorfiles@gmail.com>
2020-05-23 20:45:05 -0400

    Ignore out files

--- Utils.cpp
+++ Utils.cpp
@@ -11,6 +11,7 @@ void Options::getOptions(int argc, char* argv[]) {
   int option_index = 0;
   option long_opts[] = {
     { "verbose", no_argument, nullptr, 'v' },
+    { "help", no_argument, nullptr, 'h' },
     { "decks", required_argument, nullptr, 'd' },
     { "rounds", required_argument, nullptr, 'r' },
     { "seed", required_argument, nullptr, 's' },
@@ -18,10 +19,13 @@ void Options::getOptions(int argc, char* argv[]) {
     { nullptr, 0, nullptr, '\0' },
   };
   int opt;
-  while ((opt = getopt_long(argc, argv, "vd:r:s:p:", long_opts, &option_index)) != -1) {
+  while ((opt = getopt_long(argc, argv, "hvd:r:s:p:", long_opts, &option_index)) != -1) {
     switch (opt) {
       case 'v':
         verbose_flag = true;
+        break;
+      case 'h':
+        ERROR("Not ready yet :(");
         break;
       case 'd':
         num_decks = atoi(optarg);
